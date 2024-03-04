2e2c55caefde29c2bbe0be50a62c3e1030542411
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2024-03-04 16:26:36 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Grid } from "@mui/material";
+import { Container } from "@mui/system";
+import Navbar from "./Components/Navbar";
+/* import Result from "./Components/Result";
+import SliderSelect from "./Components/SliderSelect";
+import TenureSelect from "./Components/TenureSelect"; */
+
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar />
+    <Container maxWidth="xl" sx={{marginTop:4}}>
+      <Grid container spacing={5} alignItems="center">
+        <Grid item xs={12} md={6}>
+     {/*      <SliderSelect />
+          <TenureSelect /> */}
+        </Grid>
+        <Grid item xs={12} md={6}>
+         {/*  <Result/> */}
+        </Grid>
+      </Grid>
+    </Container>
+  </div>
+  
+  );
+}
+
+export default App;
+
