551a6a53030c635e230d3d941cccd1117656c3ab
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2024-03-08 15:24:48 -0500

    add slider labels

--- src/Components/SliderSelect.js
+++ src/Components/SliderSelect.js
@@ -1,10 +1,14 @@
+import { Typography } from '@mui/material';
 import SliderComponent from './Common/SliderComponent';
 
 const SliderSelect = () => {
   return (
     <>
+    <Typography> Slider 1 </Typography>
     <SliderComponent min={0} max={50} defaultValue={20}/>
+    <Typography> Slider 2 </Typography>
     <SliderComponent min={100} max={200} defaultValue={150}/>
+    <Typography> Slider 3</Typography>
     <SliderComponent min={0} max={10} defaultValue={2}/>
     </>
   );
