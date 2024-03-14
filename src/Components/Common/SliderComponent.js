750afce70237d788493a65225d946f6f347993ea
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2024-03-14 15:07:26 -0400

    add slider args

--- src/Components/Common/SliderComponent.js
+++ src/Components/Common/SliderComponent.js
@@ -1,8 +1,38 @@
-import Slider from '@mui/material/Slider';
+import React from "react";
+import Slider from "@mui/material/Slider";
+import { Typography } from "@mui/material";
+import { Stack } from "@mui/system";
 
-const SliderComponent = (props) => {
+const SliderComponent = ({
+defaultValue, min, max, label, unit, onChange, amount, value, steps }) => {
   return (
-  <Slider defaultValue={props.defaultValue} min={props.min} max={props.max} aria-label="Default" valueLabelDisplay="auto" />
+    <Stack my={1.4}>
+      <Stack gap={1}>
+        <Typography variant="subtitle2">{label}</Typography>
+        <Typography variant="h5">
+          {unit} {amount}
+        </Typography>
+      </Stack>
+      <Slider
+        min={min}
+        max={max}
+        defaultValue={defaultValue}
+        aria-label="Default"
+        valueLabelDisplay="auto"
+        value={value}
+        onChange={onChange}
+        marks
+        step={steps}
+      />
+      <Stack direction="row" justifyContent="space-between">
+        <Typography variant="caption" color="text.secondary">
+          {unit} {min}
+        </Typography>
+        <Typography variant="caption" color="text.secondary">
+          {unit} {max}
+        </Typography>
+      </Stack>
+    </Stack>
   );
 };
 
